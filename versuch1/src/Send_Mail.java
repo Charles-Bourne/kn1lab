411d7b6cfa09aba5ba9115507b3b35019dec1bbb
Jannis Halder <hive1.aelgdatacenter.com@jannis-halder.de>
2023-11-02 02:14:14 +0100

    Send_Mail

--- versuch1/src/Send_Mail.java
+++ versuch1/src/Send_Mail.java
@@ -14,7 +14,25 @@ public class Send_Mail {
 	
 	public static void sendMail() {
 		try {
-			// your code here
+			// Setzen der Properties und deren Werte
+			Properties props = new Properties();
+			props.put("mail.smtp.host", "localhost");
+			props.put("mail.smtp.port", "25");
+			props.put("mail.smtp.auth", "false");
+			props.put("mail.smtp.starttls.enable", "false");
+
+			// Session-Objekt erstellen
+			Session session = Session.getInstance(props);
+
+			// Erstellen der Nachricht
+			Message message = new MimeMessage(session);
+			message.setFrom(new InternetAddress("from-labrat@localhost"));
+			message.setRecipients(Message.RecipientType.TO, InternetAddress.parse("labrat@localhost"));
+			message.setSubject("Testnachricht");
+			message.setText("Hallo Welt!");
+
+			// Nachricht senden
+			Transport.send(message);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
